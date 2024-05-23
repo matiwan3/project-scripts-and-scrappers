edbcc402de8d2e2438adbaa2ea19941b64fc3ba5
Mateusz <wanmatedzlewicz@gmail.com>
2024-05-23 11:54:08 +0200

    added help menu - adjusted commands new exe created v.3

--- john talks/main.py
+++ john talks/main.py
@@ -1,37 +1,40 @@
-# JohnTalks | v.0.2
 import speech_recognition as sr
 import os
 
 # Initialize recognizer
 r = sr.Recognizer()
 
-# Function to listen for trigger phrase
+def instructions():
+    os.system("cls")
+    print("[*] 'close' - shutdown the PC\n[*] 'hibernate' - hibernate the PC\n[*] 'lock' - locks the PC\n[*] 'adios' - closes JohnTalks\n[*] 'help' - prints the help menu\n")
+    
 def listen_for_trigger():
+    print("[*] Welcome to JohnTalks | v.3 | This version supports only windows platform\n[*] To interact with the program make sure that your microphone is unmuted and ready to listen. Say 'help' for instructions.")
     while True:
         with sr.Microphone() as source:
-            print("Listening for trigger phrase...")
-            audio = r.listen(source, phrase_time_limit=2) # phrase_time_limit prevents script from infinitely waiting for phrase 
+            print("[+] Listening for trigger phrase...")
+            audio = r.listen(source, phrase_time_limit=2.5)  # listening timeframe = 2.5 sec
             
             try:
-                # Use Google Web Speech API for speech recognition
                 text = r.recognize_google(audio).lower()
-                print("You said:", text)
-                if "john banana" in text:
-                    print("Turning off PC...")
+                print("[*] You said:", text)
+                if "close" in text:
+                    print("[*] Turning off PC...")
                     os.system("shutdown /s /t 1")
                     break 
-                elif "john hibernate" in text:
-                    print("Hibernating PC...")
+                elif "hibernate" in text:
+                    print("[*] Hibernating PC...")
                     os.system("shutdown /h")
+                elif "lock" in text:
+                    print("[*] Locking PC...")
+                    os.system("rundll32.exe user32.dll,LockWorkStation")
+                elif "adios" in text:
                     break
-                elif "john apple" in text:
-                    print("Locking PC...")
-                    os.system("rundll32.exe user32.dll,LockWorkStation") # Program is still listening after locking down the PC, although needs some adjustments
-                elif "bye john" in text:
-                    break
+                elif "help" in text:
+                    instructions()
             except sr.UnknownValueError:
-                print("Could not understand audio")
+                print("[*] Could not understand audio")
             except sr.RequestError as e:
-                print("Could not request results; {0}".format(e))
+                print("[*] Could not request results; {0}".format(e))
 
 listen_for_trigger()
